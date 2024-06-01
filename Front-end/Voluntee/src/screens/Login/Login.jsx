e3705d63a44ae17734c6f445eacb3baa269cc22e
merbach020202 <eduardo.brenn2006@gmail.com>
2024-05-31 22:54:24 -0300

    sparkles: Componente de Login

--- Front-end/Voluntee/src/screens/Login/Login.jsx
+++ Front-end/Voluntee/src/screens/Login/Login.jsx
@@ -1,8 +1,24 @@
 import { ConteinerGeral } from "../../components/Container/Conteiner";
+import { Input } from "../../components/Input/Input";
+import { Titulo } from "../../components/Titulo/Titulo";
 
 export default function Login() {
     return (
         <ConteinerGeral>
+
+            <Titulo>
+                Login
+            </Titulo>
+
+            <Input
+                alter
+            >                
+            </Input>
+
+            <Input
+                alter
+            >                
+            </Input>
             
         </ConteinerGeral>
     )

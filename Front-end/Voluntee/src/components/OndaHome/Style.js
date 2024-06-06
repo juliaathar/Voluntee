ec212e39c5ca56bff1fc3a8fe536d4c6196f4982
juliaathar <juathar@gmail.com>
2024-06-06 10:42:44 -0300

    :wastebasket: Removendo pasta Front-End
    Problemas com conflitos de dependencias

--- Front-end/Voluntee/src/components/OndaHome/Style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import Svg from 'react-native-svg';
-
-export const SvgBack = styled(Svg)`
-    position: absolute;
-    z-index: -1;
-    top: 0%;
-`
--- Front-end/Voluntee/src/screens/Testes/joao.jsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native"
-
-export const Joao = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "space-between", backgroundColor: "#0066FF", flexDirection: "row" }}>
-            
-        </View>
-    )
-}

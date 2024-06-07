eb1104752567d1459768e08a9acba095667d0035
zAlves31 <alvesbautistajoaovictor@gmail.com>
2024-06-07 10:54:21 -0300

    :construction:Funcionalidade de Verificar Codigo
    Funcionalidade envia o codigo para o banco porem, sem requisicao para validar a troca de senha

--- Front-end/Voluntee/src/screens/VerificarEmail/VerificarEmail.jsx
+++ Front-end/Voluntee/src/screens/VerificarEmail/VerificarEmail.jsx
@@ -9,11 +9,46 @@ import { Input } from '../../components/Input/Input';
 import { Botao } from '../../components/Botao/Botao';
 import { Container, ContainerInput, ConteinerBottom, ConteinerButton, ConteinerLink } from '../../components/Container/Style';
 import { Link } from '../../components/Link/Link';
-import { InputVerify } from './Style';
+import { ContentVerify, InputVerify } from './Style';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useRef, useState } from 'react';
+import api from '../../service/ApiService';
 
 
-export const VerificarEmail = () => {
+export const VerificarEmail = ({navigation, route}) => {
+
+    const [codigo, setCodigo] = useState('')
+
+    const inputs = [useRef(null), useRef(null), useRef(null), useRef(null)]
+
+    function focusNextInput(index) {
+        // se o index e menor que a quantidade de campos 
+        if (index < inputs.length - 1) {
+            inputs[index + 1].current.focus()
+        }
+    }
+
+    function focusPrevInput(index) {
+        if (index > 0) {
+            inputs[index - 1].current.focus()
+        }
+    }
+
+    async function ValidarCodigo() {
+        console.log(codigo);
+
+        await api.post(`/RecuperarSenha/ValidarCodigoRecuperacaoSenha?email=${route.params.emailRecuperacao}&codigo=${codigo}`)
+            .then(() => {
+                navigation.replace("RedefinirSenha", { emailRecuperacao: route.params.emailRecuperacao });
+            }).catch(error => {
+                console.log(error);
+            })
+    }
+
+    useEffect(() => {
+        inputs[0].current.focus()
+    }, [])
+
     return (
         <Container style={styles.container}>
             <ConteinerBolaMenor>
@@ -28,33 +63,48 @@ export const VerificarEmail = () => {
 
             <Paragrafo>Digite o código que foi enviado para seu email:</Paragrafo>
 
-            <ContainerInput>
-                <InputVerify
-                    maxLength={1}
-                    placeholder= "0"
-                    keyboardType="numeric"
-                ></InputVerify>
-                <InputVerify
-                    maxLength={1}
-                    placeholder= "0"
-                    keyboardType="numeric"
-                ></InputVerify>
-                <InputVerify
-                    maxLength={1}
-                    placeholder= "0"
-                    keyboardType="numeric"
-                ></InputVerify>
-                <InputVerify
-                    maxLength={1}
-                    placeholder= "0"
-                    keyboardType="numeric"
-                ></InputVerify>
-            </ContainerInput>
+
+            <ContentVerify>
+                {
+                    [0, 1, 2, 3].map((index) => (
+                        <InputVerify
+                            key={index} //Chave de acordo com o index do map
+                            ref={inputs[index]} // Referencia de acordo
+                            maxLength={1}
+                            placeholder="0"
+                            keyboardType="numeric"
+                            caretHidden={true}
+
+                            onChangeText={(text) => {
+                                //verificar se o texto nao e vazio(para voltar para o campo anterior)
+                                if (text == "") {
+                                    focusPrevInput(index)
+
+                                } else {
+
+                                    const novoCodigo = [...codigo] //separa os valores 
+                                    novoCodigo[index] = text
+                                    setCodigo(novoCodigo.join(''))
+
+                                    // setCodigo(`${codigo}${text}`)
+
+                                    //verificar se o campo tem 1 caracter
+                                    focusNextInput(index)
+                                }
+
+
+                            }}
+                        />
+                    ))
+                }
+            </ContentVerify>
+
 
             <ConteinerButton>
 
                 <Botao
                     textoBotao='Continuar'
+                    onPress={() => ValidarCodigo()}
                 />
 
             </ConteinerButton>
